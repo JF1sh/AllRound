6c12a87306d39ebf8d0e5d1c453f8db9aeb3187d
JF1sh <309413923@qq.com>
2020-04-19 16:06:22 +0800

    建表建库信息

---
+++ doc/sql/ar_user.sql
@@ -0,0 +1,16 @@
+
+--创建数据库
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`allround` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+--创建用户表
+DROP TABLE IF EXISTS `ar_user`;
+CREATE TABLE `ar_user` (
+  `id` int(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_code` int(8) NOT NULL COMMENT '用户账号',
+  `user_name` varchar(20) NOT NULL COMMENT '用户姓名',
+  `user_password` varchar(20) NOT NULL COMMENT '用户密码',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--用户信息插入
+insert  into `ar_user`(`id`,`user_code`,`user_name`,`user_password`) values (1,55500001,'李径宇','123'),(2,55500002,'宗滔','123');
--- src/main/java/cn/allRound/dream/service/UserService.java
+++ src/main/java/cn/allRound/dream/service/UserService.java
@@ -1,10 +1,9 @@
 package cn.allRound.dream.service;
 
 import cn.allRound.dream.domain.User;
-import cn.allRound.dream.mapper.UserMapper;
 import cn.allRound.dream.dto.UserDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import cn.allRound.dream.mapper.UserMapper;
+import org.apache.log4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -15,7 +14,7 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+    private static Logger log = Logger.getLogger(UserService.class);
 
     @Resource
     private UserMapper userMapper;
@@ -28,11 +27,11 @@ public class UserService {
         public UserDto doLogin(UserDto dto) {
         List<User> userList = userMapper.findByLoginName(dto.getLoginName());
         if (CollectionUtils.isEmpty(userList)) {
-            LOG.info("根据用户名查找不到记录");
+            log.info("根据用户名查找不到记录");
             return null;
         } else {
             User user = userList.get(0);
-            LOG.info("根据用户名查找结果：{}", user);
+            log.info("根据用户名查找结果：{"+user+"}");
             return validatePassword(dto, user);
         }
     }
@@ -42,7 +41,7 @@ public class UserService {
             BeanUtils.copyProperties(user, userDto);
             return userDto;
         } else {
-            LOG.warn("密码错：{}，{}", userDto.getPassword(), user.getPassword());
+            log.error("密码错误:"+user.getName()+":"+user.getPassword());
             return null;
         }
     }
